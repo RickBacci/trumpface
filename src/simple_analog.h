73f53b78285917623b274ca8b14f4d8fcf234b36
Rick Bacci <rickbacci@gmail.com>
2016-03-12 22:37:14 -0500

    initial commit

---
+++ src/simple_analog.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "pebble.h"
+
+# define NUM_CLOCK_TICKS 11
+
+static const GPathInfo MINUTE_HAND_POINTS = {
+  6, (GPoint []) {
+    { 0, -20 },
+    { -4, 2 },
+    { 0, 5 },
+    { 4, 2 },
+    { 0, -20 },
+    { 0, 5 }
+  }
+};
+
+static const GPathInfo HOUR_HAND_POINTS = {
+  6, (GPoint []){
+    { 0, -15 },
+    { -4, 2 },
+    { 0, 5 },
+    { 4, 2 },
+    { 0, -15 },
+    { 0, 5 }
+  }
+};
